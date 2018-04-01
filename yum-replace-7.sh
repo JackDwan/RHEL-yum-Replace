8565511357ae1bdda21dffd2fc0efb6d7be55208
JackDwan <37508114+JackDwan@users.noreply.github.com>
2018-04-01 19:39:01 +0800

    Special Version for RHEL7

--- yum-replace-7.sh
+++ yum-replace-7.sh
@@ -20,19 +20,19 @@ cd /tmp/yumupdate
 #cd rhel7
 #cp CentOS-Base.repo /etc/yum.repos.d/
 wget http://mirrors.163.com/.help/CentOS7-Base-163.repo -P /etc/yum.repos.d/
-sed -i "s//$releasever/6/g" /etc/yum.repos.d/CentOS7-Base-163.repo
+sed -i "s//$releasever/7/g" /etc/yum.repos.d/CentOS7-Base-163.repo
  
 wget http://mirrors.163.com/centos/7/os/x86_64/Packages/python-iniparse-0.4-9.el7.noarch.rpm
-rpm -ivh python-iniparse-0.4-9.el7.noarch.rpm
+rpm -ivh python-iniparse-0.4-9.el7.noarch.rpm --force --nodeps
  
 wget http://mirrors.163.com/centos/7/os/x86_64/Packages/yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
-rpm -ivh yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
+rpm -ivh yum-metadata-parser-1.1.4-10.el7.x86_64.rpm --force --nodeps
  
 wget http://mirrors.163.com/centos/7/os/x86_64/Packages/yum-3.4.3-154.el7.centos.noarch.rpm
 
 wget http://mirrors.163.com/centos/7/os/x86_64/Packages/yum-plugin-fastestmirror-1.1.31-42.el7.noarch.rpm                                   
                                              
-rpm -ivh yum-3.4.3-154.el7.centos.noarch.rpm yum-plugin-fastestmirror-1.1.31-42.el7.noarch.rpm 
+rpm -ivh yum-3.4.3-154.el7.centos.noarch.rpm yum-plugin-fastestmirror-1.1.31-42.el7.noarch.rpm --force --nodeps
          
 if [ $? -eq 0  ]
   then
